175f1a92d4a504cd00dd0912be4d389ac6828307
Heather Mac <heatherlinmac@gmail.com>
2019-01-07 21:44:19 -0800

    states

---
+++ src/SeasonDisplay.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SeasonDisplay = () => {
+  return <div>Hi there!</div>;
+};
+
+ReactDOM.render(<App />, document.querySelector('#root'));
---
+++ src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    // THIS IS THE ONLY TIME we do direct assignment to this.state
+    this.state = { lat: null};
+
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        //we called setstate when wanting to update state
+        this.setState({ lat: position.coords.latitude });
+      },
+      (err) => console.log(err)
+  );
+  }
+
+  //React says we have to define render!
+  render() {
+    return <div>Latitude: {this.state.lat}</div>;
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'));
